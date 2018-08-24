971a1d986d5f1a53c8cb587b934a765a3dae7a84
Nabil Yafai <nabilyafai90@gmail.com>
2018-08-24 15:56:07 -0400

    new first commit

---
+++ Gravity/FauxGravityBody.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FauxGravityBody : MonoBehaviour {
+
+
+	public FauxGravityAttractor attractor;
+	private Rigidbody m_Rigidbody;
+	private Transform myTransform;
+	// Use this for initialization
+	void Start () {
+		m_Rigidbody = GetComponent<Rigidbody>();
+		// remove rotation and gravity from
+
+		m_Rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+		m_Rigidbody.useGravity = false;
+		myTransform = transform;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		attractor.Attract(myTransform);
+	}
+}
