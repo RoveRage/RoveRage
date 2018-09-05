8fc73ec6cd560b3309cec38b237e549efc63c9c6
johnnybee4e <jon.rosado42@gmail.com>
2018-09-05 12:28:26 -0400

    Jon set up for git. added rotation to coin and consumability when tank hits coin
    Co-authored-by: Nabil <nabilyafai90@gmail.com>

---
+++ Gravity/FauxGravityProjectile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FauxGravityProjectile : MonoBehaviour {
+
+
+	public FauxGravityAttractor attractor;
+	private Rigidbody m_Shellbody;
+	private Transform myTransform;
+	// Use this for initialization
+	void Start () {
+		m_Shellbody = GetComponent<Rigidbody>();
+		// remove rotation and gravity from
+
+		m_Shellbody.constraints = RigidbodyConstraints.FreezeRotation;
+		m_Shellbody.useGravity = false;
+		myTransform = transform;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		attractor.Attract(myTransform);
+	}
+}
